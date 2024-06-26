52039cca7ff7f3a2b06c6f0e1f32d97bf956b565
xximo0 <xximoxximo0@gmail.com>
2024-06-27 00:35:05 +0100

    Create BroUrl.sh

---
+++ BroUrl.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+# Colors
+Black='\e[0;30m'
+Red='\e[0;31m'
+Green='\e[0;32m'
+Yellow='\e[0;33m'
+Blue='\e[0;34m'
+Purple='\e[0;35m'
+Cyan='\e[0;36m'
+White='\e[0;37m'
+BRed='\e[1;31m'
+BGreen='\e[1;32m'
+BYellow='\e[1;33m'
+BPurple='\e[1;35m'
+
+# Function to create SSH tunnel using Serveo
+create_tunnel() {
+    echo "Creating SSH tunnel using Serveo..."
+    ssh -R 80:localhost:3000 serveo.net
+}
+
+# Main script
+echo -ne '\033c'
+trap RM_HT_FOLDER SIGINT SIGQUIT SIGTSTP
+echo ""
+sleep 0.1
+echo -e "${Cyan}    +${Yellow}--------------------------------------------------------------------------------------------------------------------------${Cyan}+"
+sleep 0.1
+echo -e "                                     |${BRed} Online Browser ${BYellow}by${BGreen} Hamza Hammouch${Cyan} powered by${BPurple} linuxserver${Yellow} |"
+sleep 0.1
+echo -e "                                     ${Cyan}+${Yellow}--------------------------------------------------------${Cyan}+"
+sleep 0.1
+
+#######################################################
+
+echo -e "${Yellow}     +${White}-------------------------------------------------------------------${Yellow}+"
+echo -e "${White}     | ${Yellow} ID ${White} |                   ${BPurple}   Browser Name                       ${White}   |"
+echo -e "${Yellow}     +${White}-------------------------------------------------------------------${Yellow}+"
+echo -e "${White}     | ${Red}[${Yellow}01${Red}]${White} |$Green Install Chromium${White}                                           |"
+echo -e "${White}     | ${Red}[${Yellow}02${Red}]${White} |$Green Install Firefox${White}                                            |"
+echo -e "${White}     | ${Red}[${Yellow}03${Red}]${White} |$Green Install Opera${White}                                              |"
+echo -e "${White}     | ${Red}[${Yellow}04${Red}]${White} |$Green Install Mullvad Browser${White}                                    |"
+echo -e "${White}     | ${Red}[${Yellow}05${Red}]${White} |$Green Install Microsoft Edge${White}                                      |"
+echo -e "${Yellow}     +${White}-------------------------------------------------------------------${Yellow}+"
+echo ""
+echo -e -n "$White    ${Red} [${Cyan}!Note:${Red}]$White If your choice is Chromium type $Green 1${White} not ${Red}01$White and the same principle applies to other browsers "
+echo ""
+echo ""
+echo -e -n "$White    ${Red} [${Cyan}!${Red}]$White Type the$BRed ID$White "
+read -p "of your choice : " choice
+case $choice in
+    1)
+        echo "Installing Chromium..."
+        docker run -d \
+            --name=chromium \
+            --security-opt seccomp=unconfined \
+            -e PUID=1000 \
+            -e PGID=1000 \
+            -e TZ=Etc/UTC \
+            -p 3000:3000 \
+            -p 3001:3001 \
+            -v /chromium:/config \
+            --shm-size="7gb" \
+            --restart unless-stopped \
+            ghcr.io/linuxserver/chromium:latest
+        create_tunnel  # Create SSH tunnel after starting Chromium container
+        ;;
+    2)
+        echo "Installing Firefox..."
+        docker run -d \
+            --name=firefox \
+            --security-opt seccomp=unconfined \
+            -e PUID=1000 \
+            -e PGID=1000 \
+            -e TZ=Etc/UTC \
+            -p 3000:3000 \
+            -p 3001:3001 \
+            -v /firefox:/config \
+            --shm-size="7gb" \
+            --restart unless-stopped \
+            ghcr.io/linuxserver/firefox:latest
+        create_tunnel  # Create SSH tunnel after starting Firefox container
+        ;;
+    3)
+        echo "Installing Opera..."
+        docker run -d \
+            --name=opera \
+            --security-opt seccomp=unconfined \
+            -e PUID=1000 \
+            -e PGID=1000 \
+            -e TZ=Etc/UTC \
+            -p 3000:3000 \
+            -p 3001:3001 \
+            -v /opera:/config \
+            --shm-size="7gb" \
+            --restart unless-stopped \
+            ghcr.io/linuxserver/opera:latest
+        create_tunnel  # Create SSH tunnel after starting Opera container
+        ;;
+    4)
+        echo "Installing Mullvad Browser..."
+        docker run -d \
+            --name=mullvad-browser \
+            --security-opt seccomp=unconfined \
+            -e PUID=1000 \
+            -e PGID=1000 \
+            -e TZ=Etc/UTC \
+            -p 3000:3000 \
+            -p 3001:3001 \
+            -v /mullvad-browser:/config \
+            --shm-size="7gb" \
+            --restart unless-stopped \
+            ghcr.io/linuxserver/mullvad-browser:latest
+        create_tunnel  # Create SSH tunnel after starting Mullvad Browser container
+        ;;
+    5)
+        echo "Installing Microsoft Edge..."
+        docker run -d \
+            --name=edge \
+            --security-opt seccomp=unconfined \
+            -e PUID=1000 \
+            -e PGID=1000 \
+            -e TZ=Etc/UTC \
+            -p 3000:3000 \
+            -p 3001:3001 \
+            -v /edge:/config \
+            --shm-size="7gb" \
+            --restart unless-stopped \
+            ghcr.io/linuxserver/edge:latest
+        create_tunnel  # Create SSH tunnel after starting Microsoft Edge container
+        ;;
+    *)
+        echo "Invalid choice. Please enter a number from 1 to 5."
+        exit 1
+        ;;
+esac
+
+# Print SSH tunnel information
+echo ""
+echo -e "${Yellow}SSH tunnel information:"
+echo -e "${White}Access your container via SSH tunnel using the following command:"
+echo -e "${Green}ssh -p 80 serveo.net"
+echo ""
